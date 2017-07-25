bc20305054f04edf4f0960f931c0c128c80f127c
Zong-Ying <christsaizyt@gmail.com>
2017-07-25 14:13:47 -0700

    Initial Commit

---
+++ Assessments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assessments
+//
+//  Created by ZONG-YING Tsai on 2017/7/25.
+//  Copyright © 2017年 com.ppAkcotSomeD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
